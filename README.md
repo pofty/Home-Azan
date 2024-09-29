fb1d98381c3bf8565ec3f901a19d833943b484f5
pofty <info@hasanwazzan.com>
2024-09-29 20:44:55 +0100

    changes to disconnect behaviour

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
- pip install -r .\requirements.txt
+ 
+sudo apt-get install libasound2-dev
+pip install -r .\requirements.txt
  
 to list bluetooth devices' mac addresses:
 bluetoothctl devices
--- run_commands.py
+++ run_commands.py
@@ -12,6 +12,11 @@ def set_volume(volume: int):
 def connect_to_google_mini():
     print("Connecting to Google")
     #run_command("bluetoothctl connect F8:0F:F9:A0:7C:3F")
+    # check if already connected to google mini, if not then connect
+    # get bluetooth devices connected
+    # check if google mini is connected
+    # if not then connect
+
     run_command("bluetoothctl connect 34:AF:B3:03:E3:FD")
     time.sleep(0.4)
 
@@ -20,4 +25,4 @@ def connect_to_google_mini():
 def disconnect_from_google_mini():
     print("Google mini is disconnected now")
     #run_command("bluetoothctl disconnect F8:0F:F9:A0:7C:3F")
-    run_command("bluetoothctl disconnect 34:AF:B3:03:E3:FD")
+    #run_command("bluetoothctl disconnect 34:AF:B3:03:E3:FD")
