721e746dae7d152aff09a3127dd645cc6519aad4
pofty <info@hasanwazzan.com>
2024-09-29 20:15:56 +0100

    change device mac address

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
  pip install -r .\requirements.txt
  
+to list bluetooth devices' mac addresses:
+bluetoothctl devices
+
--- run_commands.py
+++ run_commands.py
@@ -1,5 +1,5 @@
 import os
-
+import time
 
 def run_command(command: str):
     os.system(command)
@@ -11,9 +11,13 @@ def set_volume(volume: int):
 
 def connect_to_google_mini():
     print("Connecting to Google")
-    run_command("bluetoothctl connect F8:0F:F9:A0:7C:3F")
+    #run_command("bluetoothctl connect F8:0F:F9:A0:7C:3F")
+    run_command("bluetoothctl connect 34:AF:B3:03:E3:FD")
+    time.sleep(0.4)
+
 
 
 def disconnect_from_google_mini():
     print("Google mini is disconnected now")
-    run_command("bluetoothctl disconnect F8:0F:F9:A0:7C:3F")
+    #run_command("bluetoothctl disconnect F8:0F:F9:A0:7C:3F")
+    run_command("bluetoothctl disconnect 34:AF:B3:03:E3:FD")
